3db21253c8b31f63588490e9da27a18db6e78a68
Jagyosenee <jagyoseneepaul@gmail.com>
2025-08-08 16:24:47 +0530

    Update palindrome_string.py

--- palindrome_string.py
+++ palindrome_string.py
@@ -1,7 +1,8 @@
 #Write a program to check if a given string is a palindrome (same forward and backward).
-c
+str=input("Enter a string:")
 rev_of_str=str[::-1]
 if str==rev_of_str:
     print("The string is palindrome")
 else:
     print("The string is not palindrome")
+
