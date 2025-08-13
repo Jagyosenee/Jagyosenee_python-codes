a4a9ed10a1cf4844db8b9e7a57400501e7d8f5f5
Jagyosenee <jagyoseneepaul@gmail.com>
2025-08-13 20:37:11 +0530

    second_characters_of_string.py

--- second_characters_of_string.py
+++ second_characters_of_string.py
@@ -1,3 +1,4 @@
 #Write a program to print every second character of a string, starting from index 0.
 str=input("Enter the string:")
-print("Second character of the string is:",str[1])
+
+print("Second character of the string is:",str[0::2])
